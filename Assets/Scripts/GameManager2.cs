a3b9298f077fef4744d4a78954d15fbd0323d08d
Amirmahdi <gm.ta.tahmasebi@gmail.com>
2024-10-12 13:01:23 +0330

    board is updating via logic

--- Assets/Scripts/GameManager2.cs
+++ Assets/Scripts/GameManager2.cs
@@ -44,55 +44,93 @@ public class GameManager2 : SNetworkPlayer
             }
         }
 
-        for (int j = 0; j < 8; j++)
+        int whitePawn = 0, blackPawn = 8, whiteRook = 16, blackRook = 18, whiteKnight = 20, blackKnight = 22, whiteBishop = 24
+            , blackBishop = 26, whiteQueen = 28, blackQueen = 29, whiteKing = 30, blackKing = 31;
+        Piece[,] board = logic.GetBoard();
+        for(int i = 0; i < 8; i++)
         {
-            chessPieces[j] = Instantiate(whitePawnPrefab,
-                new Vector3(returnPos(j, 1).x, Tile.tileHeight, returnPos(j, 1).z), Quaternion.identity);
-            chessPieces[j + 8] = Instantiate(blackPawnPrefab,
-                new Vector3(returnPos(j, 6).x, Tile.tileHeight, returnPos(j, 6).z), Quaternion.identity);
+            for(int j = 0; j < 8; j++)
+            {
+                switch (board[i, j].Type)
+                {
+                    case PieceType.Pawn:
+                        if (board[i,j].Color == PieceColor.White)
+                        {
+                            chessPieces[whitePawn++] = Instantiate(whitePawnPrefab,
+                                new Vector3(returnPos(j, i).x, Tile.tileHeight, returnPos(j, i).z), Quaternion.identity);
+                        }
+                        else
+                        {
+                            chessPieces[blackPawn++] = Instantiate(blackPawnPrefab,
+                                new Vector3(returnPos(j, i).x, Tile.tileHeight, returnPos(j, i).z), Quaternion.identity);
+                        }
+                        break;
+                    case PieceType.Rook:
+                        if (board[i, j].Color == PieceColor.White)
+                        {
+                            chessPieces[whiteRook++] = Instantiate(whiteRookPrefab,
+                                new Vector3(returnPos(j, i).x, Tile.tileHeight, returnPos(j, i).z), Quaternion.identity);
+                        }
+                        else
+                        {
+                            chessPieces[blackRook++] = Instantiate(blackRookPrefab,
+                                new Vector3(returnPos(j, i).x, Tile.tileHeight, returnPos(j, i).z), Quaternion.identity);
+                        }
+                        break;
+                    case PieceType.Knight:
+                        if (board[i, j].Color == PieceColor.White)
+                        {
+                            chessPieces[whiteKnight++] = Instantiate(whiteKnightPrefab,
+                                new Vector3(returnPos(j, i).x, Tile.tileHeight, returnPos(j, i).z), Quaternion.identity);
+                            chessPieces[whiteKnight - 1].transform.localScale = new Vector3(chessPieces[whiteKnight - 1].transform.localScale.x,
+                                chessPieces[whiteKnight - 1].transform.localScale.y, chessPieces[whiteKnight - 1].transform.localScale.z * -1);
+                        }
+                        else
+                        {
+                            chessPieces[blackKnight++] = Instantiate(blackKnightPrefab,
+                               new Vector3(returnPos(j, i).x, Tile.tileHeight, returnPos(j, i).z), Quaternion.identity);
+                        }
+                        break;
+                    case PieceType.Bishop:
+                        if (board[i, j].Color == PieceColor.White)
+                        {
+                            chessPieces[whiteBishop++] = Instantiate(whiteBishopPrefab,
+                               new Vector3(returnPos(j, i).x, Tile.tileHeight, returnPos(j, i).z), Quaternion.identity);
+                        }
+                        else
+                        {
+                            chessPieces[blackBishop++] = Instantiate(blackBishopPrefab,
+                               new Vector3(returnPos(j, i).x, Tile.tileHeight, returnPos(j, i).z), Quaternion.identity);
+                        }
+                        break;
+                    case PieceType.Queen:
+                        if (board[i, j].Color == PieceColor.White)
+                        {
+                            chessPieces[whiteQueen++] = Instantiate(whiteQueenPrefab,
+                               new Vector3(returnPos(j, i).x, Tile.tileHeight, returnPos(j, i).z), Quaternion.identity);
+                        }
+                        else
+                        {
+                            chessPieces[blackQueen++] = Instantiate(blackQueenPrefab,
+                               new Vector3(returnPos(j, i).x, Tile.tileHeight, returnPos(j, i).z), Quaternion.identity);
+                        }
+                        break;
+                    case PieceType.King:
+                        if (board[i, j].Color == PieceColor.White)
+                        {
+                            chessPieces[whiteKing++] = Instantiate(whiteKingPrefab,
+                               new Vector3(returnPos(j, i).x, Tile.tileHeight, returnPos(j, i).z), Quaternion.identity);
+                        }
+                        else
+                        {
+                            chessPieces[blackKing++] = Instantiate(blackKingPrefab,
+                               new Vector3(returnPos(j, i).x, Tile.tileHeight, returnPos(j, i).z), Quaternion.identity);
+                        }
+                        break;
+
+                }
+            }
         }
-
-        chessPieces[16] = Instantiate(whiteRookPrefab,
-            new Vector3(returnPos(0, 0).x, Tile.tileHeight, returnPos(0, 0).z), Quaternion.identity);
-        chessPieces[17] = Instantiate(whiteRookPrefab,
-            new Vector3(returnPos(7, 0).x, Tile.tileHeight, returnPos(7, 0).z), Quaternion.identity);
-        chessPieces[18] = Instantiate(blackRookPrefab,
-            new Vector3(returnPos(0, 7).x, Tile.tileHeight, returnPos(0, 7).z), Quaternion.identity);
-        chessPieces[19] = Instantiate(blackRookPrefab,
-            new Vector3(returnPos(7, 7).x, Tile.tileHeight, returnPos(7, 7).z), Quaternion.identity);
-
-        chessPieces[20] = Instantiate(whiteKnightPrefab,
-            new Vector3(returnPos(1, 0).x, Tile.tileHeight, returnPos(1, 0).z), Quaternion.identity);
-        chessPieces[21] = Instantiate(whiteKnightPrefab,
-            new Vector3(returnPos(6, 0).x, Tile.tileHeight, returnPos(6, 0).z), Quaternion.identity);
-        chessPieces[20].transform.localScale = new Vector3(chessPieces[20].transform.localScale.x,
-            chessPieces[20].transform.localScale.y, chessPieces[20].transform.localScale.z * -1);
-        chessPieces[21].transform.localScale = new Vector3(chessPieces[21].transform.localScale.x,
-            chessPieces[21].transform.localScale.y, chessPieces[21].transform.localScale.z * -1);
-
-        chessPieces[22] = Instantiate(blackKnightPrefab,
-            new Vector3(returnPos(1, 7).x, Tile.tileHeight, returnPos(1, 7).z), Quaternion.identity);
-        chessPieces[23] = Instantiate(blackKnightPrefab,
-            new Vector3(returnPos(6, 7).x, Tile.tileHeight, returnPos(6, 7).z), Quaternion.identity);
-
-        chessPieces[24] = Instantiate(whiteBishopPrefab,
-            new Vector3(returnPos(2, 0).x, Tile.tileHeight, returnPos(2, 0).z), Quaternion.identity);
-        chessPieces[25] = Instantiate(whiteBishopPrefab,
-            new Vector3(returnPos(5, 0).x, Tile.tileHeight, returnPos(5, 0).z), Quaternion.identity);
-        chessPieces[26] = Instantiate(blackBishopPrefab,
-            new Vector3(returnPos(2, 7).x, Tile.tileHeight, returnPos(2, 7).z), Quaternion.identity);
-        chessPieces[27] = Instantiate(blackBishopPrefab,
-            new Vector3(returnPos(5, 7).x, Tile.tileHeight, returnPos(5, 7).z), Quaternion.identity);
-
-        chessPieces[28] = Instantiate(whiteQueenPrefab,
-            new Vector3(returnPos(3, 0).x, Tile.tileHeight, returnPos(3, 0).z), Quaternion.identity);
-        chessPieces[29] = Instantiate(blackQueenPrefab,
-            new Vector3(returnPos(3, 7).x, Tile.tileHeight, returnPos(3, 7).z), Quaternion.identity);
-
-        chessPieces[30] = Instantiate(whiteKingPrefab,
-            new Vector3(returnPos(4, 0).x, Tile.tileHeight, returnPos(4, 0).z), Quaternion.identity);
-        chessPieces[31] = Instantiate(blackKingPrefab,
-            new Vector3(returnPos(4, 7).x, Tile.tileHeight, returnPos(4, 7).z), Quaternion.identity);
     }
 
    public static Coordinate ReturnCoordinate(float x, float z)
